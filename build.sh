6e8bcd1f974d04ef96d01e26045874555e7de86f
Andres-0304 <andresfer0304@gmail.com>
2025-06-18 15:56:18 -0500

    despliegue2

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Build script for Render
-echo "Building EzPark Platform with Maven..."
-./mvnw clean package -DskipTests=true
-echo "Build completed successfully!"
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Start script for Render
-echo "Starting EzPark Platform..."
-java -Dspring.profiles.active=prod -jar target/ezpark-platform-0.0.1-SNAPSHOT.jar
