0d3741ccde6efc10262a3ef0048754aee24bf21a
Andres-0304 <andresfer0304@gmail.com>
2025-06-20 11:40:23 -0500

    userId

--- src/main/java/com/acme/ezpark/platform/review/interfaces/rest/resources/ParkingRatingSummaryResource.java
+++ src/main/java/com/acme/ezpark/platform/review/interfaces/rest/resources/ParkingRatingSummaryResource.java
@@ -1,6 +1,5 @@
 package com.acme.ezpark.platform.review.interfaces.rest.resources;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 /**
@@ -11,15 +10,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "Parking Rating Summary Resource")
 public record ParkingRatingSummaryResource(
         @Schema(description = "Parking ID", example = "1")
-        @JsonProperty("parking_id")
         Long parkingId,
         
         @Schema(description = "Average rating", example = "4.25")
-        @JsonProperty("average_rating")
         Double averageRating,
         
         @Schema(description = "Total number of reviews", example = "12")
-        @JsonProperty("review_count")
         Long reviewCount
 ) {
 }
