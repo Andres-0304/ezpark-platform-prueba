9b452a80efb54acf5742bdd39089f1678b1aa2db
Andres-0304 <andresfer0304@gmail.com>
2025-06-22 03:54:41 -0500

    clearrr

--- src/main/java/com/acme/ezpark/platform/booking/domain/model/commands/CancelBookingCommand.java
+++
@@ -1,7 +0,0 @@
-package com.acme.ezpark.platform.booking.domain.model.commands;
-
-public record CancelBookingCommand(
-    Long bookingId,
-    String cancellationReason
-) {
-}
--- src/main/java/com/acme/ezpark/platform/booking/domain/model/commands/CompleteBookingCommand.java
+++
@@ -1,6 +0,0 @@
-package com.acme.ezpark.platform.booking.domain.model.commands;
-
-public record CompleteBookingCommand(
-    Long bookingId
-) {
-}
